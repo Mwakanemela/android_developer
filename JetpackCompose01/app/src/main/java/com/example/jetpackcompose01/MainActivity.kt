6fc3179f7e52ccc0a50a05b3b5ef59f0845aba8e
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-09-28 22:24:52 +0200

    Add whatever project

--- JetpackCompose01/app/src/main/java/com/example/jetpackcompose01/MainActivity.kt
+++ JetpackCompose01/app/src/main/java/com/example/jetpackcompose01/MainActivity.kt
@@ -5,8 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,33 +25,68 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose01.ui.theme.JetpackCompose01Theme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackCompose01Theme {
-                var count by remember {
-                    mutableStateOf(0)
+                //declaring state
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
                 }
                 Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    //need a column to cover our whole background
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
                 ) {
-                    Text(
-                        text = count.toString(),
-                        fontSize = 30.sp    
-                        )
-                    Button(onClick = {
-                    count++
-                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
                     ) {
-                      Text(text = "Count + 1")  
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .weight(1f),
+                            value = name,
+                            onValueChange = {
+                                text ->
+                                name = text
+                            }
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(
+                            onClick = {
+                                if(name.isNotBlank()) {
+                                    names += name
+                                    name = ""
+                                }
+                            }
+                        ) {
+                            Text(text ="Add")
+                        }
                     }
-            
+
+                    LazyColumn{
+                        items(names) {
+                            currentName ->
+                            Text(
+                                text = currentName,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            )
+                            Divider()
+                        }
+                    }
+
+
                 }
             }
         }
