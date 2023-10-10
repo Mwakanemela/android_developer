db590e7324e1a88af8501aa40f3bec66ee7e7d45
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-10-10 05:53:04 +0200

    chaife site using webview

--- TrobadohSites/app/src/main/java/com/example/trobadohsites/MainActivity.kt
+++ TrobadohSites/app/src/main/java/com/example/trobadohsites/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.trobadohsites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView : WebView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://trobadohsites.000webhostapp.com/")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(true)
     }
 }
