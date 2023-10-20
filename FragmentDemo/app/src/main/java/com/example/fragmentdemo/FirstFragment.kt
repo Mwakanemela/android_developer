864d771dec648a604a4b7e2c6d89e9be97a3dafb
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-10-20 23:21:02 +0200

    adding widgets on a fragment - button and textview

--- FragmentDemo/app/src/main/java/com/example/fragmentdemo/FirstFragment.kt
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 class FirstFragment : Fragment() {
 
@@ -14,7 +16,15 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        val btnFr1: Button = view.findViewById(R.id.fr1Btn)
+        val tvFr1: TextView = view.findViewById(R.id.tvfr1)
+
+        btnFr1.setOnClickListener {
+            tvFr1.text = "Change text to get text"
+        }
+        return view
     }
 
 }
