ea44bff44ea28f960486e1b4af2f77264252da55
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-10-20 22:49:48 +0200

    Navigate from 1 fragment to another

--- FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.kt
+++ FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.fragmentdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        val mainActivityTv: TextView = findViewById(R.id.inActivityTv)
+        val btn1: Button = findViewById(R.id.firstFragmentButton)
+        val btn2: Button = findViewById(R.id.secondFragmentButton)
+
+        btn1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayout, firstFragment)
+                commit()
+
+                mainActivityTv.visibility = INVISIBLE
+            }
+        }
+        btn2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayout, secondFragment)
+                commit()
+
+                mainActivityTv.visibility = INVISIBLE
+            }
+        }
     }
 }
