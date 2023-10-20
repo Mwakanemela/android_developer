98b332d7df8cdabd087440be2fe382c4ea4acdc7
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-10-21 00:36:32 +0200

    Pass data btwn fragments

--- FragmentDemo2/app/src/main/java/com/example/fragmentdemo2/MainActivity.kt
+++ FragmentDemo2/app/src/main/java/com/example/fragmentdemo2/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.fragmentdemo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayout, firstFragment)
+            addToBackStack(null)
+            commit()
+
+        }
     }
 }
