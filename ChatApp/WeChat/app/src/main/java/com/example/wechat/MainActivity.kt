8e62334c44da4f3589b64e96b8bad2bdbe8059b5
Mwakanemela Kayange <kayangemwakanemela@gmail.com>
2023-12-10 07:39:46 +0200

    from main activity go to register activity after 3 seconds

---
+++ ChatApp/WeChat/app/src/main/java/com/example/wechat/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.wechat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.Timer
+import kotlin.concurrent.schedule
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        Timer().schedule(3000) {
+            startActivity(Intent(applicationContext, RegisterActivity::class.java))
+            finish()
+        }
+    }
+}
